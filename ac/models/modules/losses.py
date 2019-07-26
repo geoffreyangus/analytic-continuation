7e63cadbeb10eb5a0711c81a14c54cac5996e19e
Geoffrey L. Angus <glangus@us.ibm.com>
2019-07-25 19:19:11 -0700

    added: mt soft cross entropy loss

--- ac/models/modules/losses.py
+++ ac/models/modules/losses.py
@@ -19,37 +19,97 @@ class MTMSELoss(MSELoss):
         """
         losses = self._compute_loss(inputs, targets)
         loss = self._balance(losses, targets)
-        # print('--')
-        # print(f'final loss:\t{loss}')
         return loss
 
     def _compute_loss(self, inputs, targets):
         """
         """
-        # print('--')
-        # print('MTMSELoss')
         losses = {}
-        for task, Y in targets.items():
+        for task in targets.keys():
             Y_hat = inputs[task]
-            # print(f"task: {task}")
-            # print(f'- true:\t{Y.data}')
-            # print(f'- pred:\t{Y_hat.data}')
-            losses[task] = super().forward(Y_hat, Y)
-            # print(f"- loss:\t{losses[task]}")
+            losses[task] = super().forward(Y_hat, targets[task])
         return losses
 
     def _balance(self, losses, targets):
         """
         """
-        # print('--')
-        for task, Y in targets.items():
-            # broadcast multiply task-specific coefficients (uses all targets)
+        for task in targets.keys():
             if self.reduction == "mean":
                 losses[task] = losses[task].mean()
             if self.reduction == "sum":
                 losses[task] = losses[task].sum()
-            # print(f'task: {task}')
-            # print(f'- loss:\t{losses[task]}')
 
         loss = sum(losses.values())
         return loss
+
+
+class MTSoftCrossEntropyLoss(nn.Module):
+    """Computes the CrossEntropyLoss while accepting probabilistic (float)
+    targets in a multi-task setting. This is a modified implementation of
+    SoftCrossEntropyLoss as presented by the HazyResearch group at Stanford.
+
+    Source: https://github.com/HazyResearch/metal/
+
+    Args:
+        weight: a tensor of relative weights to assign to each class.
+            the kwarg name 'weight' is used to match CrossEntropyLoss
+        reduction: how to combine the elementwise losses
+            'none': return an unreduced list of elementwise losses
+            'mean': return the mean loss per elements
+            'sum': return the sum of the elementwise losses
+    Accepts:
+        input: An [n, k] float tensor of prediction logits (not probabilities)
+        target: An [n, k] float tensor of target probabilities
+    """
+
+    def __init__(self, weight=None, reduction="mean"):
+        super().__init__()
+        # Register as buffer is standard way to make sure gets moved /
+        # converted with the Module, without making it a Parameter
+        if weight is None:
+            self.weight = None
+        else:
+            # Note: Sets the attribute self.weight as well
+            self.register_buffer("weight", torch.FloatTensor(weight))
+        self.reduction = reduction
+
+    def forward(self, inputs, targets):
+        """
+        """
+        losses = self._compute_loss(inputs, targets)
+        loss = self._balance(losses, targets)
+        return loss
+
+    def _compute_loss(self, inputs, targets):
+        """
+        """
+        losses = {}
+        for task in targets.keys():
+            n, k = inputs[task].shape
+            # Note that t.new_zeros, t.new_full put tensor on same device as t
+            cum_losses = inputs[task].new_zeros(n)
+            for y in range(k):
+                cls_idx = inputs[task].new_full((n,), y, dtype=torch.long)
+                y_loss = F.cross_entropy(inputs[task], cls_idx, reduction="none")
+                if self.weight is not None:
+                    y_loss = y_loss * self.weight[y]
+                cum_losses += target[task][:, y].float() * y_loss
+            losses[task] = cum_losses
+        return losses
+
+    def _balance(self, losses, targets):
+        """
+        """
+        for task in targets.keys():
+            if self.reduction == "none":
+                continue
+            elif self.reduction == "mean":
+                losses[task] = losses[task].mean()
+            elif self.reduction == "sum":
+                losses[task] = losses[task].sum()
+            else:
+                raise ValueError(f"Unrecognized reduction: {self.reduction}")
+
+        loss = sum(losses.values())
+        return loss
+
